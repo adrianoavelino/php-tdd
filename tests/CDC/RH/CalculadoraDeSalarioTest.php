e0cb2b693e23b85a9f3ca56f4fc8a21cab27bae0
Adriano Avelino <adriano-st777@hotmail.com>
2016-01-12 22:27:18 -0200

    adding baby steps in page 62

---
+++ tests/CDC/RH/CalculadoraDeSalarioTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CDC\Loja\RH;
+
+require './vendor/autoload.php';
+
+use PHPUnit_Framework_TestCase as PHPUnit;
+use CDC\Loja\RH\CalculadoraDeSalario,
+ CDC\Loja\RH\Funcionario,
+ CDC\Loja\RH\TabelaCargos;
+
+
+class CalculadoraDeSalarioTest extends PHPUnit{
+
+    public function testCalculoSalarioDesenvolvedoresComSalarioAbaixoDoLimite()  
+    {
+        $calculadora = new CalculadoraDeSalario();
+        $desenvolvedor = new Funcionario("André", 1500.0, TabelaCargos::DESENVOLVEDOR);
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+        
+        $this->assertEquals(1500.0 * 0.9, $salario, null, 0.00001);
+        
+    }
+
+    public function testCalculoSalarioDesenvolvedoresComSalarioAcimaDoLimite() 
+    {
+    
+        $calculadora = new CalculadoraDeSalario();
+        $desenvolvedor = new Funcionario("André", 4000, TabelaCargos::DESENVOLVEDOR);
+        $salario = $calculadora->calculaSalario($desenvolvedor);
+        
+        $this->assertEquals(4000 * 0.8, $salario, null, 0.00001);
+    }
+        
+    
+    
+}
