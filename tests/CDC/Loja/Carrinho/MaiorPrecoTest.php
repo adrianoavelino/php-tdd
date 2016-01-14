ebb1a537b891a46b7617a8e6e3955d2e702f6f33
Adriano Avelino <adriano-st777@hotmail.com>
2016-01-14 12:11:08 -0200

    chapter 05 - page 70

--- tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
+++ tests/CDC/Loja/Carrinho/MaiorPrecoTest.php
@@ -2,6 +2,7 @@
 namespace CDC\Loja\Carrinho;
 
 use CDC\Loja\Test\TestCase,
+ CDC\Loja\Produto\Produto,
  CDC\Loja\Carrinho\CarrinhoDeCompras,
  CDC\Loja\Carrinho\MaiorPreco;
 
@@ -17,4 +18,29 @@ class MaiorPrecoTest extends TestCase
         
     }
     
+    public function testeDeveRetornarValorItemSeCarrinhoCom1Elemento() 
+    {
+        $carrinho = new CarrinhoDeCompras();
+        $carrinho->adiciona(new Produto("Geladeira", 900.00, 1));
+        
+        $algoritmo = new MaiorPreco();
+        $valor = $algoritmo->encontra($carrinho);
+        
+        
+        $this->assertEquals(900.00, $valor, null, 0.0001);
+    }
+    
+    public function testDeveRetornarMaiorValorSeCarrinhoComMuitosElementos() 
+    {
+        $carrinho = new CarrinhoDeCompras();
+        
+        $carrinho->adiciona(new Produto("Fogão", 1, 1500.00));
+        $carrinho->adiciona(new Produto("Geladeira", 1, 900.00));
+        $carrinho->adiciona(new Produto("Máquina de lavar", 1, 750.00));
+        
+        $algoritmo = new MaiorPreco();
+        $valor = $algoritmo->encontra($carrinho);
+        
+        $this->assertEquals(1500.00, $valor, null, 0.0001);
+    }
 }
