df53976d8755e91b2f8ff8eadd3a145fdb0d28d5
Adriano Avelino <adriano-st777@hotmail.com>
2016-01-19 11:58:31 -0200

    updating CalculadoraDeSalario - page 101

--- src/CDC/Loja/RH/CalculadoraDeSalario.php
+++ src/CDC/Loja/RH/CalculadoraDeSalario.php
@@ -9,22 +9,30 @@ class CalculadoraDeSalario
     public function calculaSalario(Funcionario $funcionario) 
     {
         if ($funcionario->getCargo() === TabelaCargos::DESENVOLVEDOR) {
-
-            if ($funcionario->getSalario() > 3000) {
-                return $funcionario->getSalario() * 0.8;
-            }
-            return $funcionario->getSalario() * 0.9;
+            return $this->dezOuVintePorCentoDeDesconto($funcionario);
+            
         } elseif ($funcionario->getCargo() === TabelaCargos::DBA || 
                 $funcionario->getCargo() === TabelaCargos::TESTADOR) {
-            
-            if ($funcionario->getSalario() < 2500 ) {
-                return $funcionario->getSalario() * 0.85;
-            }
-            return $funcionario->getSalario() * 0.75;
+            return $this->quinzeOuVintePorCentoDeDesconto($funcionario);
         }
         
         throw new \RuntimeException("Tipo de funcionário inválido");
     }
+    
+    public function dezOuVintePorCentoDeDesconto(Funcionario $funcionario) 
+    {
+        if ($funcionario->getSalario() > 3000) {
+            return $funcionario->getSalario() * 0.8;
+        }
+        return $funcionario->getSalario() * 0.9;
+    }
         
+    public function quinzeOuVintePorCentoDeDesconto(Funcionario $funcionario)
+    {
+        if ($funcionario->getSalario() < 2500 ) {
+            return $funcionario->getSalario() * 0.85;
+        }
+        return $funcionario->getSalario() * 0.75;        
+    }
     
 }
