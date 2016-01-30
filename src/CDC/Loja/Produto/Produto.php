fed399406d4c9e0bf372a2808316ab23ad2e8d68
Adriano Avelino <adriano-st777@hotmail.com>
2016-01-30 17:55:45 -0200

    updating - chapter 1 - page 165

--- src/CDC/Loja/Produto/Produto.php
+++ src/CDC/Loja/Produto/Produto.php
@@ -7,11 +7,13 @@ class Produto
     private$nome;
     private $valorUnitario;
     private $quantidade;
+    private $status = true;
 
     /**
      * @codeCoverageIgnore
      * @param type $nome
-     * @param type $valor
+     * @param type $valorUnitario
+     * @param type $quantidade
      */
     public function __construct($nome, $valorUnitario, $quantidade = 1)
     {
@@ -24,22 +26,31 @@ class Produto
     {
         return $this->nome;
     }
-    
-    function getValorUnitario() 
+
+    function getValorUnitario()
     {
         return $this->valorUnitario;
     }
 
-    function getQuantidade() 
+    function getQuantidade()
     {
         return $this->quantidade;
     }
 
-    public function getValorTotal() 
+    public function getValorTotal()
     {
         return $this->valorUnitario * $this->quantidade;
     }
     
+    public function getStatus()
+    {
+        return $this->status;
+    }
+    
+    public function inativa()
+    {
+        $this->status = false;
 
+    }
 
 }
